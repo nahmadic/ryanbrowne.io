796165a32bd7f6f30c0ac6c0549e568268071d99
Ryan Browne <brownetown7@gmail.com>
2019-09-02 21:21:52 +0100

    Adds background image

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,6 @@
 import React from "react"
+import { useStaticQuery, graphql }from "gatsby"
+import Img from "gatsby-image"
 
 import Header from "./header"
 
@@ -6,8 +8,24 @@ import "../styles/main.scss"
 
 const Layout = (props) => {
 
+  const data = useStaticQuery(graphql`
+    query {
+      backgroundImage: file(relativePath: { eq: "street.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 2560) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
   return (
     <div className="page-container">
+      <div className="page-container__background">
+        <Img fluid={data.backgroundImage.childImageSharp.fluid} />
+        <div className="page-container__overlay"></div>
+      </div>
       <Header />
       <div className="page-content">
         {props.children}
