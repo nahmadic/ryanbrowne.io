8d5c6c43845343a6ba53a58934c57f1051d156e6
Ryan Browne <brownetown7@gmail.com>
2019-09-02 21:52:16 +0100

    Adds icons for social media

--- src/components/header.js
+++ src/components/header.js
@@ -1,29 +1,42 @@
 import React from "react"
 import { Link } from "gatsby";
 
-const Header = () => (
-  <header className="header">
-    <div className="header__logo">
-      <Link to="/">ryanbrowne</Link>
-    </div>
-    <div className="header__links">
-      <div className="header__link">
-        <a href="mailto:hello@ryanbrowne.io?subject=Let's%20work%20together!" target="_blank"><img src="./assets/img/gmail.svg" alt="Email" /></a>
+import gmailIcon from '../images/gmail.svg'
+import githubIcon from '../images/github.svg'
+import linkedinIcon from '../images/linkedin.svg'
+import twitterIcon from '../images/twitter.svg'
+
+const Header = () => {
+
+  return (
+    <header className="header">
+      <div className="header__logo">
+        <Link to="/">ryanbrowne</Link>
       </div>
-      <div className="header__link">
-        <a href="mailto:hello@ryanbrowne.io?subject=Let's%20work%20together!" target="_blank"><img src="./assets/img/gmail.svg" alt="Email" /></a>
+      <div className="header__links">
+        <div className="header__link">
+          <a href="mailto:hello@ryanbrowne.io?subject=Let's%20work%20together!" target="_blank">
+            <img src={gmailIcon} alt="Email" />
+          </a>
+        </div>
+        <div className="header__link">
+          <a href="https://github.com/nahmadic" target="_blank">
+            <img src={githubIcon} alt="GitHub" />
+          </a>
+        </div>
+        <div className="header__link">
+          <a href="https://www.linkedin.com/in/ryan-s-browne/" target="_blank">
+            <img src={linkedinIcon} alt="Linkedin" />
+          </a>
+        </div>
+        <div className="header__link">
+          <a href="https://twitter.com/nahmadic" target="_blank">
+            <img src={twitterIcon} alt="Twitter" />
+          </a>
+        </div>
       </div>
-      <div className="header__link">
-        <a href="mailto:hello@ryanbrowne.io?subject=Let's%20work%20together!" target="_blank"><img src="./assets/img/gmail.svg" alt="Email" /></a>
-      </div>
-      <div className="header__link">
-        <a href="mailto:hello@ryanbrowne.io?subject=Let's%20work%20together!" target="_blank"><img src="./assets/img/gmail.svg" alt="Email" /></a>
-      </div>
-      <div className="header__link">
-        <a href="mailto:hello@ryanbrowne.io?subject=Let's%20work%20together!" target="_blank"><img src="./assets/img/gmail.svg" alt="Email" /></a>
-      </div>
-    </div>
-  </header>
-)
+    </header>
+  )
+}
 
 export default Header
