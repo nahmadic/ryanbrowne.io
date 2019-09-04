fdc39255f231f65efeb545343941ce341e0e90c1
Ryan Browne <brownetown7@gmail.com>
2019-09-04 21:55:34 +0100

    Adds temporary link for tag line

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => (
     <SEO title="Home" />
     <div className="index__content">
       <h1>Hey, I'm Ryan.</h1>
-      <h2>I'm a front-end developer. <br/> Check out some of the stuff I've done.</h2>
+      <h2>I'm a front-end developer. <br/> Check out some of the <a href="https://github.com/nahmadic" target="_blank">stuff I've done</a>.</h2>
     </div>
   </Layout>
 )
