e54b7993033a6dd4cd559ae749a714a4fd1d6e79
Ryan Browne <brownetown7@gmail.com>
2019-09-04 21:41:10 +0100

    Changes social icons and colours

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,6 @@
 import React from "react"
 import { Link } from "gatsby";
 
-import gmailIcon from '../images/gmail.svg'
-import githubIcon from '../images/github.svg'
-import linkedinIcon from '../images/linkedin.svg'
-import twitterIcon from '../images/twitter.svg'
-
 const Header = () => {
 
   return (
@@ -14,25 +9,17 @@ const Header = () => {
         <Link to="/">ryanbrowne</Link>
       </div>
       <div className="header__links">
-        <div className="header__link">
-          <a href="mailto:hello@ryanbrowne.io?subject=Let's%20work%20together!" target="_blank">
-            <img src={gmailIcon} alt="Email" />
-          </a>
+        <div className="header__link header__link--twitter">
+          <a href="https://twitter.com/nahmadic" target="_blank">tw</a>
         </div>
-        <div className="header__link">
-          <a href="https://github.com/nahmadic" target="_blank">
-            <img src={githubIcon} alt="GitHub" />
-          </a>
+        <div className="header__link header__link--github">
+          <a href="https://github.com/nahmadic" target="_blank">gh</a>
         </div>
-        <div className="header__link">
-          <a href="https://www.linkedin.com/in/ryan-s-browne/" target="_blank">
-            <img src={linkedinIcon} alt="Linkedin" />
-          </a>
+        <div className="header__link header__link--linkedin">
+          <a href="https://www.linkedin.com/in/ryan-s-browne/" target="_blank">li</a>
         </div>
-        <div className="header__link">
-          <a href="https://twitter.com/nahmadic" target="_blank">
-            <img src={twitterIcon} alt="Twitter" />
-          </a>
+        <div className="header__link header__link--email">
+          <a href="mailto:hello@ryanbrowne.io?subject=Let's%20work%20together!" target="_blank">em</a>
         </div>
       </div>
     </header>
